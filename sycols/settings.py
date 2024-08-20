b39f23b3d89214c3caca4a61a577da7ed2b13270
mohsenpiri61 <mopiry@gmail.com>
2024-08-20 16:51:03 +0330

    end

--- sycols/settings.py
+++ sycols/settings.py
@@ -178,9 +178,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [BASE_DIR / "staticfiles",]
